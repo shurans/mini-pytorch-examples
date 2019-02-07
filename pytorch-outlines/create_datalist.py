864398660e8eda4664ebd0ac043405dbac472584
Shreeyak <ss.shrek7@gmail.com>
2019-02-08 00:11:24 +0530

    updated datalist

--- pytorch-outlines/create_datalist.py
+++ pytorch-outlines/create_datalist.py
@@ -1,5 +1,5 @@
 f = open("data/datalist", "w+")
 for i in range(0,100):
-    f.write("train/rgb-imgs/%09d-rgb.jpg,train/edges-imgs/%09d-segmentation.png\n"%(i, i))
+    f.write("train/rgb-imgs/%09d-rgb.jpg,train/combined-edges/%09d-segmentation.png\n"%(i, i))
 
 f.close()
